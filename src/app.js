56e5631567c5cc223ec9857dcdb0e98b9f2feb57
Rozpi <100348581+Rozpide@users.noreply.github.com>
2024-11-20 20:30:20 +0000

    primeros cambios en generator excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+function generateExcuse() {
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let actionRandom = action[Math.floor(Math.random()) * action.length];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  return `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`;
+}
 window.onload = function() {
-  //write your code here
+  document.getElementById("excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
 };
